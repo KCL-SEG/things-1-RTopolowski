95fcfa79c163d447e935c21d4ee23f33a630c220
RTopolowski <robert.topolowski@gmail.com>
2022-10-17 00:05:19 +0100

    Fix description validation error

--- things/models.py
+++ things/models.py
@@ -6,7 +6,9 @@ from django.core import validators
 
 class Thing(models.Model):
     name = models.CharField(max_length=30, unique=True)
-    description = models.CharField(max_length=120)
+    description = models.CharField(
+        max_length=120, validators=[validators.MinLengthValidator(1)]
+    )
     quantity = models.IntegerField(
         validators=[validators.MinValueValidator(0), validators.MaxValueValidator(100)]
     )
